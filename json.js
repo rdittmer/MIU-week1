2a9b535de3c90f09f6e262f7bc1e4fb450e4192a
Ryan Dittmer <rdittmer@fullsail.edu>
2012-06-01 15:36:09 -0400

    added project files, working additem and index, json needs updated for new project

---
+++ json.js
@@ -0,0 +1,162 @@
+var json = {
+	"Bill1":{
+		"billtype":  ["Bill Type:", "Utilities"],
+		"payto":     ["Pay To:", "City of Jacksonville"],
+		"payamount": ["Pay Amount:", "60.00"],
+		"paywith":   ["Pay With:", "Bank Account"],
+		"paydate":   ["Pay Date:", "04/11/2012"],
+		"notes":     ["Notes:", "Water"]
+	},
+	"Bill2":{
+		"billtype":  ["Bill Type:", "Auto"],
+		"payto":     ["Pay To:", "Navy Fed"],
+		"payamount": ["Pay Amount:", "271.00"],
+		"paywith":   ["Pay With:", "Credit Card"],
+		"paydate":   ["Pay Date:", "04/02/2012"],
+		"notes":     ["Notes:", "350z"]
+	}
+	"Bill3":{
+		"billtype":  ["Bill Type:", "Auto"],
+		"payto":     ["Pay To:", "Navy Fed"],
+		"payamount": ["Pay Amount:", "459.00"],
+		"paywith":   ["Pay With:", "Credit Card"],
+		"paydate":   ["Pay Date:", "04/20/2012"],
+		"notes":     ["Notes:", "Genesis Coupe"]
+	}
+	"Bill4":{
+		"billtype":  ["Bill Type:", "Utilities"],
+		"payto":     ["Pay To:", "Jefferson Elect"],
+		"payamount": ["Pay Amount:", "80.00"],
+		"paywith":   ["Pay With:", "Check"],
+		"paydate":   ["Pay Date:", "04/11/2012"],
+		"notes":     ["Notes:", "Electric"]
+	}
+	"Bill5":{
+		"billtype":  ["Bill Type:", "Rent-House"],
+		"payto":     ["Pay To:", "CTU Properties"],
+		"payamount": ["Pay Amount:", "968.00"],
+		"paywith":   ["Pay With:", "Bank Account"],
+		"paydate":   ["Pay Date:", "04/15/2012"],
+		"notes":     ["Notes:", "Rent"]
+	}
+	"Bill6":{
+		"billtype":  ["Bill Type:", "Credit-Card"],
+		"payto":     ["Pay To:", "Navy Fed"],
+		"payamount": ["Pay Amount:", "500.00"],
+		"paywith":   ["Pay With:", "Bank Account"],
+		"paydate":   ["Pay Date:", "04/08/2012"],
+		"notes":     ["Notes:", "Credit Card"]
+	}
+	"Bill7":{
+		"billtype":  ["Bill Type:", "Credit-Card"],
+		"payto":     ["Pay To:", "Star"],
+		"payamount": ["Pay Amount:", "300.00"],
+		"paywith":   ["Pay With:", "Bank Account"],
+		"paydate":   ["Pay Date:", "04/18/2012"],
+		"notes":     ["Notes:", "Star Card"]
+	}
+	"Bill8":{
+		"billtype":  ["Bill Type:", "Other"],
+		"payto":     ["Pay To:", "State Farm"],
+		"payamount": ["Pay Amount:", "60.00"],
+		"paywith":   ["Pay With:", "Credit Card"],
+		"paydate":   ["Pay Date:", "04/27/2012"],
+		"notes":     ["Notes:", "Jetta Insurance"]
+	}
+	"Bill9":{
+		"billtype":  ["Bill Type:", "Other"],
+		"payto":     ["Pay To:", "State Farm"],
+		"payamount": ["Pay Amount:", "105.00"],
+		"paywith":   ["Pay With:", "Credit Card"],
+		"paydate":   ["Pay Date:", "04/20/2012"],
+		"notes":     ["Notes:", "Genesis Insurance"]
+	}
+	"Bill10":{
+		"billtype":  ["Bill Type:", "Other"],
+		"payto":     ["Pay To:", "State Farm"],
+		"payamount": ["Pay Amount:", "90.00"],
+		"paywith":   ["Pay With:", "Credit Card"],
+		"paydate":   ["Pay Date:", "04/22/2012"],
+		"notes":     ["Notes:", "350z Insurance"]
+	}
+	"Bill11":{
+		"billtype":  ["Bill Type:", "Auto"],
+		"payto":     ["Pay To:", "Eaton CU"],
+		"payamount": ["Pay Amount:", "50.00"],
+		"paywith":   ["Pay With:", "Bank Account"],
+		"paydate":   ["Pay Date:", "04/05/2012"],
+		"notes":     ["Notes:", "Jetta"]
+	}
+	"Bill12":{
+		"billtype":  ["Bill Type:", "Utilities"],
+		"payto":     ["Pay To:", "Jackson fuels"],
+		"payamount": ["Pay Amount:", "35.00"],
+		"paywith":   ["Pay With:", "Check"],
+		"paydate":   ["Pay Date:", "04/15/2012"],
+		"notes":     ["Notes:", "gas"]
+	}
+	"Bill13":{
+		"billtype":  ["Bill Type:", "Credit Card"],
+		"payto":     ["Pay To:", "Kays"],
+		"payamount": ["Pay Amount:", "100.00"],
+		"paywith":   ["Pay With:", "Bank Account"],
+		"paydate":   ["Pay Date:", "04/08/2012"],
+		"notes":     ["Notes:", "Ring"]
+	}
+	"Bill14":{
+		"billtype":  ["Bill Type:", "Credit Card"],
+		"payto":     ["Pay To:", "BestBuy"],
+		"payamount": ["Pay Amount:", "150.00"],
+		"paywith":   ["Pay With:", "Check"],
+		"paydate":   ["Pay Date:", "04/03/2012"],
+		"notes":     ["Notes:", "TV"]
+	}
+	"Bill15":{
+		"billtype":  ["Bill Type:", "Credit Card"],
+		"payto":     ["Pay To:", "Staples"],
+		"payamount": ["Pay Amount:", "100.00"],
+		"paywith":   ["Pay With:", "Check"],
+		"paydate":   ["Pay Date:", "04/07/2012"],
+		"notes":     ["Notes:", "Desk"]
+	}
+	"Bill16":{
+		"billtype":  ["Bill Type:", "Other"],
+		"payto":     ["Pay To:", "Navy Fed"],
+		"payamount": ["Pay Amount:", "220.00"],
+		"paywith":   ["Pay With:", "Credit Card"],
+		"paydate":   ["Pay Date:", "04/27/2012"],
+		"notes":     ["Notes:", "School loan"]
+	}
+	"Bill17":{
+		"billtype":  ["Bill Type:", "Other"],
+		"payto":     ["Pay To:", "Pioneer"],
+		"payamount": ["Pay Amount:", "137.00"],
+		"paywith":   ["Pay With:", "Bank Account"],
+		"paydate":   ["Pay Date:", "04/20/2012"],
+		"notes":     ["Notes:", "Personal loan"]
+	}
+	"Bill18":{
+		"billtype":  ["Bill Type:", "Utilities"],
+		"payto":     ["Pay To:", "City of Jacksonville"],
+		"payamount": ["Pay Amount:", "20.00"],
+		"paywith":   ["Pay With:", "Check"],
+		"paydate":   ["Pay Date:", "04/10/2012"],
+		"notes":     ["Notes:", "Trash"]
+	}
+	"Bill19":{
+		"billtype":  ["Bill Type:", "Other"],
+		"payto":     ["Pay To:", "Speed Motorsports"],
+		"payamount": ["Pay Amount:", "200.00"],
+		"paywith":   ["Pay With:", "Bank Account"],
+		"paydate":   ["Pay Date:", "04/25/2012"],
+		"notes":     ["Notes:", "350z Upgrades"]
+	}
+	"Bill20":{
+		"billtype":  ["Bill Type:", "Other"],
+		"payto":     ["Pay To:", "Appliance Place"],
+		"payamount": ["Pay Amount:", "35.00"],
+		"paywith":   ["Pay With:", "Credit Card"],
+		"paydate":   ["Pay Date:", "04/13/2012"],
+		"notes":     ["Notes:", "Washer/Dryer"]
+	}
+}
